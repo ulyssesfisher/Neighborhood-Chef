76ec233c06c820e0610ca72c550a102ac1d643a0
Erwins Saget <erwinsaget@gmail.com>
2019-02-01 13:36:11 -0500

    refactored the faker js file

--- src/faker.js
+++ src/faker.js
@@ -1,33 +1,42 @@
 const $ = require("jquery");
 var faker = require('faker');
 const cuisineArray = ["American", "Greek", "Mexican","Chinese","Thai","Indian","Italian"];
-let array = [];
+const numberOfChefs = 5;
 
+const createChefProfile = function(chef) {
+    return `
+        <div id="chef-${chef.id}">
+            <img src="${chef.avatar}">
+            <div>
+                <p>${chef.name}</p>
+                <p>${chef.company}</p>
+                <p>${chef.name}</p>
+                <p>${chef.email}</p>
+                <p>${chef.website}</p>
+            </div>
+        </div>
+`};
 
-
+const createChef = function() {
+    return {
+        id: faker.random.uuid(),
+        avatar: faker.image.avatar(),
+        name: faker.name.findName(),
+        company: faker.company.companyName(),
+        email: faker.internet.email(),
+        website: faker.internet.domainName(),
+        cuisines: cuisineArray[Math.floor(Math.random() * cuisineArray.length)]
+    }
+};
 
 const createChefProfiles = function(){
-    for (let i = 0; i < 5; i++) {
-  
-      $('<img/>', {
-          src: faker.image.avatar()
-      }).appendTo('#wrapper');
-  
-      $('<div/>', {
-          text: faker.name.findName()
-      }).appendTo('#wrapper');
-  
-      $('<div/>', {
-          text: faker.company.companyName()
-      }).appendTo('#wrapper');
-  
-      $('<div/>', {
-          text: faker.internet.email()
-      }).appendTo('#wrapper');
-  
-      $('<div/>', {
-          text: faker.internet.domainName()
-      }).appendTo('#wrapper');
+    for(let i = 0; i < numberOfChefs; i++) {
+        const chef = createChef();
+
+        const profile = createChefProfile(chef);
+
+        $('#wrapper').append(profile);
+    }
   }
-  }
+
   export default createChefProfiles;
