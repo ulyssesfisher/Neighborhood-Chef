177fd7e6a0a35cecc4f10884019a5da8707aee0f
Erwins Saget <erwinsaget@gmail.com>
2019-02-04 11:01:06 -0500

    WIP

--- src/ui.js
+++ src/ui.js
@@ -34,7 +34,7 @@ const renderResultsPage = function(restaurants) {
 
   let resultsPage = `
     <nav class="navbar navbar-expand-lg navbar-dark mb-5" style="background-color: #8C4D2E;">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Neighborhood Chef</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -43,7 +43,6 @@ const renderResultsPage = function(restaurants) {
         <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
         <a class="nav-item nav-link" href="#">Features</a>
         <a class="nav-item nav-link" href="#">Pricing</a>
-        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </div>
     </div>
   </nav>
